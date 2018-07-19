59295258a9dcf24fa5f1c62b2f4e843c7eed82c7
OumaWu <1114110754@qq.com>
2018-07-20 01:29:02 +0800

    专家账号修改功能完成

--- expert_edit.php
+++ expert_edit.php
@@ -1,6 +1,6 @@
 ﻿<?
 include("admin.php");
-define('IMG_PATH', "http://".$_SERVER['HTTP_HOST']."/cattsp/user_files/expert/");
+define('IMG_PATH', "http://" . $_SERVER['HTTP_HOST'] . "/cattsp/user_files/expert/");
 require_once('sql/selectExpert.php');
 $res = $result->fetch(PDO::FETCH_OBJ);
 ?>
@@ -21,7 +21,7 @@ $res = $result->fetch(PDO::FETCH_OBJ);
         <tr>
             <td>
                 <h1>添加专家账号</h1>
-                <form id="form1" name="form1" method="post" action="./sql/insertExpert.php"
+                <form id="form1" name="form1" method="post" action="./sql/updateExpert.php"
                       enctype="multipart/form-data">
                     <!--                    <table width="100%" cellspacing="0" cellpadding="0">-->
                     <!--                        <tr>-->
@@ -30,58 +30,75 @@ $res = $result->fetch(PDO::FETCH_OBJ);
                     <!--                    </table>-->
                     <!--                    <br/>-->
                     <table width="100%" border="0" cellpadding="6" cellspacing="0" class="grid">
+                        <input type="hidden" id="id" name="id" value="<?= $res->id; ?>"/>
+                        <input type="hidden" id="old_password" name="old_password" value="<?= $res->password; ?>"/>
                         <tr>
                             <th width="120" align="right">用户名：</th>
-                            <td><input name="accountname" value="<?=$res->accountname;?>" type="text" id="accountname" size="60"/></td>
+                            <td><input name="accountname" value="<?= $res->accountname; ?>" type="text" id="accountname"
+                                       size="60"/></td>
+                        </tr>
+                        <tr>
+                            <th width="120" align="right">新密码：</th>
+                            <td><input name="password" type="password" id="password" value="" size="60"/>
+                                <ol class="breadcrumb  alert-info">
+                                    <li><a>Tips：如不填，则保持原有密码</a></li>
+                                </ol>
+                            </td>
                         </tr>
                         <tr>
                             <th align="right">头像：</th>
                             <td>
-                                <img id="headimg" src="<?=IMG_PATH.$res->photo;?>" alt="预览"
+                                <img id="headimg" src="<?= IMG_PATH . $res->photo; ?>" alt="预览"
                                      onerror="this.src='./images/default.jpg';"
                                      style="height: 150px; width: 200px;"/>
                                 <input type="file" id="photo" name="photo" accept="image/*"/>
-                                <input type="hidden" name="o_photo" id="o_photo" value="<?=$res->photo;?>">
+                                <input type="hidden" name="o_photo" id="o_photo" value="<?= $res->photo; ?>">
                             </td>
                         </tr>
                         <tr>
                             <th width="120" align="right">姓名：</th>
-                            <td><input name="name" value="<?=$res->name;?>" type="text" id="name" size="60"/></td>
+                            <td><input name="name" value="<?= $res->name; ?>" type="text" id="name" size="60"/></td>
                         </tr>
                         <tr>
                             <th width="120" align="right">职位/头衔：</th>
-                            <td><input name="title" value="<?=$res->title;?>" type="text" id="title" size="60"/></td>
+                            <td><input name="title" value="<?= $res->title; ?>" type="text" id="title" size="60"/></td>
                         </tr>
                         <tr>
                             <th width="120" align="right">所在地：</th>
-                            <td><input name="location" value="<?=$res->location;?>" type="text" id="location" size="60"/></td>
+                            <td><input name="location" value="<?= $res->location; ?>" type="text" id="location"
+                                       size="60"/></td>
                         </tr>
                         <tr>
                             <th width="120" align="right">从业时长(年)：</th>
-                            <td><input name="career_age" value="<?=$res->career_age;?>" type="text" id="career_age" size="60"/></td>
+                            <td><input name="career_age" value="<?= $res->career_age; ?>" type="text" id="career_age"
+                                       size="60"/></td>
                         </tr>
                         <tr>
                             <th width="120" align="right">学历：</th>
-                            <td><input name="degree" value="<?=$res->degree;?>" type="text" id="degree" size="60"/></td>
+                            <td><input name="degree" value="<?= $res->degree; ?>" type="text" id="degree" size="60"/>
+                            </td>
                         </tr>
                         <tr>
                             <th width="120" align="right">毕业院校：</th>
-                            <td><input name="institute" value="<?=$res->institute;?>" type="text" id="institute" size="60"/></td>
+                            <td><input name="institute" value="<?= $res->institute; ?>" type="text" id="institute"
+                                       size="60"/></td>
                         </tr>
                         <tr>
                             <th width="120" align="right">从事领域：</th>
-                            <td><input name="domain" value="<?=$res->domain;?>" type="text" id="domain" size="60"/></td>
+                            <td><input name="domain" value="<?= $res->domain; ?>" type="text" id="domain" size="60"/>
+                            </td>
                         </tr>
                         <tr>
                             <th width="120" align="right">擅长专业：</th>
-                            <td><input name="speciality" value="<?=$res->speciality;?>" type="text" id="speciality" size="60"/></td>
+                            <td><input name="speciality" value="<?= $res->speciality; ?>" type="text" id="speciality"
+                                       size="60"/></td>
                         </tr>
                         <tr>
                             <th align="right">个人简介：</th>
                             <td>
                                 <label for="introduction"></label>
                                 <textarea name="introduction" id="introduction" rows="15" cols="100"
-                                          style="overflow: scroll;"><?=$res->introduction;?></textarea>
+                                          style="overflow: scroll;"><?= $res->introduction; ?></textarea>
                             </td>
                         </tr>
                         <tr>
--- sql/updateExpert.php
+++ sql/updateExpert.php
@@ -1,8 +1,11 @@
 <?php
-define('FILE_UPLOAD_PATH', "../user_files/expert/");
-$userid = $_POST["userid"];
-$user = $_POST["user"];
-$url = "../specialist_profile.php";
+define('FILE_UPLOAD_PATH', "{$_SERVER['DOCUMENT_ROOT']}/cattsp/user_files/expert/");
+$url = "../expert_list.php";
+$url2 = $_SERVER["HTTP_REFERER"];
+$id = $_POST["id"];
+$accountname = $_POST["accountname"];
+$password = $_POST["password"];
+$old_password = $_POST["old_password"];
 $name = $_POST["name"];
 $title = $_POST["title"];
 $career_age = $_POST["career_age"];
@@ -15,12 +18,18 @@ $introduction = $_POST["introduction"];
 $new_images = (string)$_FILES['photo']['name'];
 $old_images = (string)$_POST['o_photo'];
 
-var_dump($_FILES, $new_images, $old_images);
-
-if (!empty($name) && !empty($title) && isset($career_age) && !empty($degree)
+if (!empty($accountname) && !empty($old_password) && !empty($name) && !empty($title) && isset($career_age) && !empty($degree)
     && !empty($institute) && !empty($domain) && !empty($speciality) && !empty($location) && !empty($introduction)) {
 
     include("connection.php");
+
+    //如果输入了新密码，则加密；否则将原有密码赋给新密码变量
+    if (empty($password)) {
+        $password = $old_password;
+    }
+    else {
+        $password = password_hash($password, PASSWORD_DEFAULT);
+    }
 
     // 上传修改的图片
     //如果文件名不为空，则加密文件名
@@ -30,24 +39,10 @@ if (!empty($name) && !empty($title) && isset($career_age) && !empty($degree)
         $img_ext = substr($new_images, strrpos($new_images, '.'));
 
         // 加密图片名
-        $new_images =  $user . hash_file('md5', $_FILES['photo']["tmp_name"]) .$img_ext;
+        $new_images =  $accountname . hash_file('md5', $_FILES['photo']["tmp_name"]) .$img_ext;
 
-        // 添加上传路径
-        $file_path = FILE_UPLOAD_PATH .$new_images;
-
-        // 上传新图片
-        if (!file_exists($file_path)) {
-            if (move_uploaded_file($_FILES['photo']["tmp_name"],  $file_path)) {
-
-                echo "图片" . $file_path . "上传成功！！" . "<br>";
-
-                // 删除旧图片
-                $path = FILE_UPLOAD_PATH . $old_images;
-                if (file_exists($path)) {
-                    unlink($path);
-                }
-            }
-        }
+        // 删除旧图片
+        deleteOldImg($old_images);
     }
     else {
         // 如果是空的，则把旧的文件名传给它
@@ -55,28 +50,57 @@ if (!empty($name) && !empty($title) && isset($career_age) && !empty($degree)
     }
 
     $sql = "UPDATE `specialists`"
-            ."SET `name` = '$name', `title` = '$title', `career_age` = '$career_age', `degree` = '$degree', `institute` = '$institute',"
-            ." `domain` = '$domain', `speciality` = '$speciality', `location` = '$location', `introduction` = '$introduction', `photo` = '{$new_images}'"
-            ." WHERE `specialists`.`id` = $userid";
+            ."SET `accountname` = '{$accountname}', `password` = '{$password}', `name` = '$name', `title` = '$title', "
+            ."`career_age` = '$career_age', `degree` = '$degree', `institute` = '$institute',"
+            ." `domain` = '$domain', `speciality` = '$speciality', `location` = '$location', "
+            ."`introduction` = '$introduction', `photo` = '{$new_images}'"
+            ." WHERE `specialists`.`id` = $id";
     try {
         $pdo->beginTransaction();
         $result = $pdo->prepare($sql);
-        if ($result->execute()) {
+        if ($result->execute() && uploadImg($new_images)) {
             $pdo->commit();
-            echo "<script> alert('你的资料更新成功！！');</script>";
-//            echo "<meta http-equiv=\"refresh\" content=\"0.5;url={$url}\">";
+            echo "<script> alert('专家资料更新成功！！');</script>";
+            echo "<meta http-equiv=\"refresh\" content=\"0.5;url={$url}\">";
         } else {
             $pdo->rollBack();
-            echo "<script> alert('你的资料更新失败！！');</script>";
-            echo $pdo->errorInfo();
-//            echo "<meta http-equiv=\"refresh\" content=\"0.5;url={$url}\">";
+            echo "<script> alert('专家资料更新失败！！\\n{$pdo->errorInfo()}');</script>";
+            echo "<meta http-equiv=\"refresh\" content=\"0.5;url={$url2}\">";
         }
     } catch (PDOException $e) {
         die("错误!!: " . $e->getMessage() . "<br>");
     }
 } else {
     echo "<script> alert('请填必填信息！！');</script>";
-    echo "<meta http-equiv=\"refresh\" content=\"0.5;url={$url}\">";
+    echo "<meta http-equiv=\"refresh\" content=\"0.5;url={$url2}\">";
+}
+
+function uploadImg($new_images) {
+
+    // 添加上传路径
+    $file_path = FILE_UPLOAD_PATH .$new_images;
+
+    // 上传新图片
+    if (!file_exists($file_path)) {
+
+        if (move_uploaded_file($_FILES['photo']["tmp_name"],  $file_path)) {
+//            echo "图片" . $file_path . "上传成功！！" . "<br>";
+            return true;
+        }
+        else return false;
+    }
+    return true;
+}
+
+function deleteOldImg($old_images) {
+    // 删除旧图片
+    $path = FILE_UPLOAD_PATH . $old_images;
+    if (file_exists($path)) {
+        if (unlink($path))
+            return true;
+        return false;
+    }
+    return true;
 }
 
 ?>
