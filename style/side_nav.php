8a4f0eb77531ce88a81e23b9df150443149f8e51
OumaWu <1114110754@qq.com>
2018-08-03 03:54:11 +0800

    侧边栏增加统计模块

--- style/side_nav.php
+++ style/side_nav.php
@@ -35,6 +35,17 @@
             </ul>
         </li>
 
+        <li style="border-bottom:1px solid #ddd" class="sub">
+            <a href="javascript:;">数据统计
+                <div class="pull-right"><span class="caret"></span></div>
+            </a>
+            <ul class="templatemo-submenu">
+                <li><a href="tech_statistic.php"><i class="fa fa-home"></i>技术成果统计</a></li>
+                <li><a href="demand_statistic.php"><i class="fa fa-home"></i>企业需求统计</a></li>
+                <li><a href="ranking_statistic.php"><i class="fa fa-home"></i>热点统计</a></li>
+            </ul>
+        </li>
+
         <li style="border-bottom:1px solid #ddd "><a href="javascript:void(0);" onclick="logout();">注销登录</a>
         </li>
         <script>
--- test.php
+++ test.php
@@ -5,6 +5,43 @@
 
 //$url='http://'.$_SERVER['HTTP_HOST'];
 
-echo ceil(4/10);
-
 //echo $_SERVER['PHP_SELF'];
+
+header("Content-type: text/html; charset=utf-8");
+
+//获取访客ip
+function getIp()
+{
+    $ip = false;
+    if (!empty($_SERVER["HTTP_CLIENT_IP"])) {
+        $ip = $_SERVER["HTTP_CLIENT_IP"];
+    }
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ips = explode(", ", $_SERVER['HTTP_X_FORWARDED_FOR']);
+        if ($ip) {
+            array_unshift($ips, $ip);
+            $ip = FALSE;
+        }
+        for ($i = 0; $i < count($ips); $i++) {
+            if (!eregi("^(10│172.16│192.168).", $ips[$i])) {
+                $ip = $ips[$i];
+                break;
+            }
+        }
+    }
+    return ($ip ? $ip : $_SERVER['REMOTE_ADDR']);
+}
+
+//根据ip获取城市、网络运营商等信息
+function findCityByIp($ip)
+{
+    $data = file_get_contents('http://ip.taobao.com/service/getIpInfo.php?ip=' . $ip);
+    return json_decode($data, true);
+}
+
+$ip = getIp();
+
+echo $ip . "<br>";
+
+echo "<pre>";
+print_r(findCityByIp("114.253.75.164"));
